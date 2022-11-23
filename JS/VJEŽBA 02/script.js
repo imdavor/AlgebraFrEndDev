1382789d2637b953a80db555060cf9fa820031d0
Davor <davor.kos@gmail.com>
2022-11-23 18:46:06 +0100

    sync

---
+++ JS/VJEŽBA 02/script.js
@@ -0,0 +1,13 @@
+//deklaracija varijable
+var ime; // undefined
+
+console.log(ime);
+
+//inicijalizacija varijable
+ime = "Ana";
+console.log(ime);
+
+//deklaracija i inicijalizacija
+
+var prezime = "Anić";
+console.log(ime + " " + prezime);
