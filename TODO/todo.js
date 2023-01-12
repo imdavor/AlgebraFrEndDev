bb90c1c60e7e4a2afbbaa88324a2ab97439408c4
Davor <davor.kos@gmail.com>
2023-01-12 21:22:35 +0100

    sync a

--- TODO/todo.js
+++ TODO/todo.js
@@ -6,6 +6,7 @@ export default class Todo {
     TaskList = null;
     SaveButton = null;
     DeleteButton = null;
+    StorageKey = "_tasks";
 
     constructor(root, options = {}) {
         const defaultOptions = {
@@ -23,6 +24,7 @@ export default class Todo {
         this.initDOM(root, defaultOptions);
         this.initElementObjects();
         this.initEventListeners();
+        this.checkStorage();
     };
 
     //5.  kreira potrebne DOM elemente i umetnw ih u postojeći DOM(root element)
@@ -51,114 +53,187 @@ export default class Todo {
     //7. inicijalizira sve potrebne evenListenere
     initEventListeners = () => {
         this.AddButton.addEventListener("click", this.addTaskToList);
+        this.InputField.addEventListener("keyup", this.addtaskToListOnEnter);
+        this.SaveButton.addEventListener("click", this.saveList);
+        this.DeleteButton.addEventListener("click", this.deleteList);
     };
-
-    //8.  kreiraj novi element liste kao novi task
-
-    createTaskItem = (task) => {
-        const listElement = document.createElement("li");
-        listElement.innerHTML = task;
-        listElement.dataset.createdAt = this.generateTimeStamp();
-        listElement.dataset.completedAt = "";
-
-        this.addCheckBoxtoListElement(listElement);
-        this.addRemoveBtnToListElement(listElement);
-
-        return listElement;
-    };
-
-    // 9. kreiraj checkbox element i dodaj mu eventlistener i umetni ga na početak li elementa
-
-    addCheckBoxtoListElement = (listElement) => {
-        const cb = document.createElement("input");
-        cb.setAttribute("type", "checkbox");
-        cb.addEventListener("change", this.changeTaskState);
-
-        // TODO: dodati listener
-
-        listElement.prepend(cb);
-    };
-
-    // 10. dodaj btn za brisanje elementa
-    addRemoveBtnToListElement = (listElement) => {
-        const btn = document.createElement("button");
-        btn.setAttribute("type", "button");
-        btn.innerHTML = "x";
-        listElement.append(btn);
-
-        btn.addEventListener("click", this.removeTaskItemFromList);
-    };
-
-    /*** 7.
-     *  EVENT LISTENERS
-     */
-    //  dodaj novi task na listu
-    addTaskToList = () => {
-        //console.log("radi");
-        const task = this.InputField.value.trim();
-        if (!task) {
-            alert("Upiši task!");
+    // provjeri da li ima pohranjena lista u lokalnoj pohrani.
+    // ako ima rekonstruiraj listu taskova i pokaži je
+    checkStorage = () => {
+        if (window.localStorage === undefined) {
+            alert("Tvoj preglednik ne podržava lokalnu pohranu!")
             return;
         }
 
-        const taskItem = this.createTaskItem(task);
-        this.TaskList.append(taskItem);
-        this.resetInputField(taskItem);
-    };
+        const storageData = localStorage.getItem(this.StorageKey);
 
-    // promijeni status taska
-    changeTaskState = (e) => {
-        const cb = e.target;
-        const listElement = cb.parentNode;
+        if (storageData != null) {
+            const tasks = JSON.parse(decodeURIComponent(atob(storageData)));
 
-        //console.log(cb);
-        if (cb.checked) {
-            listElement.style.textDecoration = "line-through";
-            listElement.dataset.completedAt = this.generateTimeStamp();
-        } else {
-            listElement.style.textDecoration = "none";
-        }
-    };
+            // console.log(tasks)
+            for (const task of tasks) {
+                const taskItem = this.createTaskItem(task.text);
+                taskItem.dataset.createdAt = task.createdAt;
+                taskItem.dataset.completedAt = task.completedAt;
 
-    // izbriši task s liste
-    removeTaskItemFromList = (e) => {
-        const btn = e.target;
-        const listElement = btn.parentNode;
-        const cb = listElement.querySelector("input[type=checkbox]");
-
-        if (cb.checked) {
-            alert("Nemreš izbrisati završeni task!");
-            return;
+                if (task.isDone) {
+                    taskItem.style.textDecoration = "line-through";
+                    task.querySelector("input[type=checkbox]").checked = true;
+                }
+                this.TaskList.append(taskItem);
+            }
         }
 
-        if (!confirm("Are you sure you want to delete this Task???")) {
-            return;
+        //8.  kreiraj novi element liste kao novi task
+        createTaskItem = (task) => {
+            const listElement = document.createElement("li");
+            listElement.innerHTML = task;
+            listElement.dataset.createdAt = this.generateTimeStamp();
+            listElement.dataset.completedAt = "";
+
+            this.addCheckBoxtoListElement(listElement);
+            this.addRemoveBtnToListElement(listElement);
+
+            return listElement;
+        };
+
+        // 9. kreiraj checkbox element i dodaj mu eventlistener i umetni ga na početak li elementa
+        addCheckBoxtoListElement = (listElement) => {
+            const cb = document.createElement("input");
+            cb.setAttribute("type", "checkbox");
+            cb.addEventListener("change", this.changeTaskState);
+
+            // TODO: dodati listener
+
+            listElement.prepend(cb);
+        };
+
+        // 10. dodaj btn za brisanje elementa
+        addRemoveBtnToListElement = (listElement) => {
+            const btn = document.createElement("button");
+            btn.setAttribute("type", "button");
+            btn.innerHTML = "x";
+            listElement.append(btn);
+
+            btn.addEventListener("click", this.removeTaskItemFromList);
+        };
+
+        /*** 7.
+         *  EVENT LISTENERS
+         */
+
+
+        //  dodaj novi task na listu
+        addTaskToList = () => {
+            //console.log("radi");
+            const task = this.InputField.value.trim();
+            if (!task) {
+                alert("Upiši task!");
+                return;
+            }
+
+            const taskItem = this.createTaskItem(task);
+            this.TaskList.append(taskItem);
+            this.resetInputField(taskItem);
+        };
+
+        // dodaj novi task na listu kada se pritisne tipka Enter
+        addtaskToListOnEnter = (e) => {
+            if (e.key === "Enter")
+                this.addTaskToList();
         }
 
-        listElement.remove();
-    };
+        // promijeni status taska
+        changeTaskState = (e) => {
+            const cb = e.target;
+            const listElement = cb.parentNode;
 
-    /**
-     * END EVENT LISTENERS
-     */
+            //console.log(cb);
+            if (cb.checked) {
+                listElement.style.textDecoration = "line-through";
+                listElement.dataset.completedAt = this.generateTimeStamp();
+            } else {
+                listElement.style.textDecoration = "none";
+            }
+        };
 
-    /**
-     * HELPERS
-     */
-    resetInputField = () => {
-        this.InputField.value = "";
-        this.InputField.focus();
-    };
+        // izbriši task s liste
+        removeTaskItemFromList = (e) => {
+            const btn = e.target;
+            const listElement = btn.parentNode;
+            const cb = listElement.querySelector("input[type=checkbox]");
 
-    generateTimeStamp = () => {
-        const date = new Date();
+            if (cb.checked) {
+                alert("Nemreš izbrisati završeni task!");
+                return;
+            }
 
-        return `${date.getFullYear()}-${
-            date.getMonth() + 1
-        }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    };
+            if (!confirm("Are you sure you want to delete this Task???")) {
+                return;
+            }
 
-    /**
-     * END HELPERS
-     */
+            listElement.remove();
+        };
+
+        // pohrani listu u lokalnu pohranu
+        saveList = () => {
+            if (window.localStorage === undefined) {
+                alert("Tvoj preglednik ne podržava lokalnu pohranu!")
+                return;
+            }
+            const taskItems = this.TaskList.childNodes;
+            // console.log(taskItems);
+            if (taskItems.length === 0) {
+                alert("Nisi dodao niti jedan task !");
+                return;
+            }
+            const tasks = [];
+            for (const task of taskItems) {
+                // console.log(task.childNodes)
+                tasks.push({
+                    isDone: task.querySelector("input[type=checkbox]").checked,
+                    text: task.childNodes[1].data,
+                    createdAt: task.dataset.createdAt,
+                    completedAt: task.dataset.completedAt
+                });
+            }
+            // console.log(tasks);
+            localStorage.setItem = (this.StorageKey,
+                btoa(encodeURIComponent(JSON.stringify(tasks))));
+            // serijalizacija  JSON.stringify(tasks)
+        }
+        // izbriši listu iz HTML DOM-a i lokalne pohrane
+        deleteList = () => {
+            if (!confirm("Da li si siguran da želiš izbrisati lokalnu listu?")) {
+                return;
+            }
+            localStorage.removeItem(this.StorageKey);
+            this.TaskList.innerHTML = "";
+
+            alert("Uspješno si  izbrisao listu!");
+        }
+
+        /**
+         * END EVENT LISTENERS
+         */
+
+        /**
+         * HELPERS
+         */
+        resetInputField = () => {
+            this.InputField.value = "";
+            this.InputField.focus();
+        };
+
+        generateTimeStamp = () => {
+            const date = new Date();
+
+            return `${date.getFullYear()}-${date.getMonth() + 1
+                }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        };
+
+        /**
+         * END HELPERS
+         */
+    }
 }
