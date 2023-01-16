3d2f62746fdd4b13874875a8e7d5ee43ae3fee36
Davor <davor.kos@gmail.com>
2023-01-16 21:01:51 +0100

    sync a

---
+++ RAZVOJNA OKOLINA/01/Webpack/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const oath = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'
+    },
+    mode: 'development'
+}
