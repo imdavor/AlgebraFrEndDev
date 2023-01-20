8422336e8a643bf64eaf392b140417dc0376f704
Davor <davor.kos@gmail.com>
2023-01-20 19:15:47 +0100

    sync a

--- RAZVOJNA OKOLINA/03/express.js/app.js
+++ RAZVOJNA OKOLINA/03/express.js/app.js
@@ -1,6 +1,28 @@
 const express = require('express');
 // kreiaraj app
-const app = express;
+const app = express();
 const port = 3000;
 
-app.listen();
+//4.
+const users = [
+    { name: 'Ana', age: 25 },
+    { name: 'Mirko', age: 35 },
+];
+
+
+//3. dođe zahtjev na port 3000 i ovo je root servera
+// jedan je request korisnika, res je response 
+app.get('/', (req, res) => {
+    res.send('Ovo server odgovara...')
+})
+
+//4.1. daj sve korsinike
+app.get('/users', (req, res) => {
+    res.send(JSON.stringify(users));
+})
+
+//2.  digni server na portu i čekaj request
+// u gitbash opali node app.je i pokazuje App on port...
+app.listen(port, () => {
+    console.log(`App on port ${port}`);
+});
