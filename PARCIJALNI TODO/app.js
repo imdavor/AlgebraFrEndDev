4a280619afc18d74b7b874e283a314e650efc7d5
Davor <davor.kos@gmail.com>
2023-01-29 14:56:17 +0100

    sync d

--- PARCIJALNI TODO/app.js
+++ PARCIJALNI TODO/app.js
@@ -1,19 +1,23 @@
 // Selectors
-/** 1. pokupi sve sa indexa */
+/** 1. pokupi klase  */
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo'); // select element
+
 
 // Event Listeners
 /** 2. kad kliknem dodaj Todo  */
+document.addEventListener('DOMContentLoaded', getTodos); // 10. ovime checkiramo da li se stranica učitala, ako je > exec funkc getTodos
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // Functions
 /** 3. */
 function addTodo(event) {
     /** spriječi formu da submita */
-    event.preventDefault();
+    event.preventDefault(); // (todoInput.value)
     // console.log('hello')
     /** start creating <li> strukturu koja će se umetnuti unutar <ul class="todo-list">*/
     /** biti će
@@ -27,16 +31,16 @@ function addTodo(event) {
     const todoDiv = document.createElement("div");
     // kreiraj i dodaj klasu elementu (div-u)
     todoDiv.classList.add('todo');
-
     // kreiraj LI
     const newTodo = document.createElement('li');
     // vrijednost koju hoću unutra 
     newTodo.innerText = todoInput.value;
     // dodaj klasu
     newTodo.classList.add('todo-item');
-    // zalijepi u Div todo-item
+    // zalijepi todo-item u Div 
     todoDiv.appendChild(newTodo); // sad imamo div sa li item-om
-
+    // 8.1  dodajemo TODO u local storage
+    saveLocalTodos();
     // sad trebamo 2 buttona
     // CHECK MARK BUTTON
     const completedButton = document.createElement('button');
@@ -73,6 +77,8 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         // za animaciju dodamo klasu fall prije remove elemnta
         todo.classList.add("fall"); // problem je kaj remove odma remova element, zato dodajemo funk da čeka na remove dok animacija ne završi
+        // nakon što smo dodali fall , brišemo iz local storeage-a
+        removeLocalTodos(todo);
         todo.addEventListener('transitionend', function () {
             todo.remove();
         });
@@ -82,4 +88,110 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
+}
+
+// FILTER
+function filterTodo(e) {
+    // zgrabi sve todos childNodes
+    const todos = todoList.childNodes;
+    // console.log(todos);
+    // kreiramo loop da kada klikamo na Option samo item iz optiona je klikable
+    todos.forEach(function (todo) {
+        // targettiramo value is option elemenata
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex"; // show all
+                break;
+            case "completed":
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';  // show all
+                } else {
+                    todo.style.display = 'none'; // show none
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';  // show all
+                } else {
+                    todo.style.display = 'none'; // show none
+                }
+                break;
+        }
+    })
+}
+
+/// 8. save to local storage
+function saveLocalTodos(todo) {
+    //1. CHECK // provjeriti da li već imamo neki todos
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = []; // ako nema kreiraj prazan array
+    } else {
+        // a ako ima parsiraj u array
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    /// grab todo i gurni ih u todos array
+    todos.push(todo);
+    // push back in to local storage
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+/// 9. bez ovoga ne radi dodavanje todoa na index.
+function getTodos() {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = []; // ako nema kreiraj prazan array
+    } else {
+        // a ako ima parsiraj u array
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    // 10. moramo rekreirati sve što se gore događaloi
+    todos.forEach(function (todo) {
+        // kreiraj Todo DIV
+        const todoDiv = document.createElement("div");
+        // kreiraj i dodaj klasu elementu (div-u)
+        todoDiv.classList.add('todo');
+        // kreiraj LI
+        const newTodo = document.createElement('li');
+        newTodo.innerText = todo; // ovdje ne trebamo više value iz inputa nego trebamo value iz local storage-a
+        newTodo.classList.add('todo-item');
+        todoDiv.appendChild(newTodo); // sad imamo div sa li item-om
+
+        // sad trebamo 2 buttona
+        // CHECK MARK BUTTON
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<i class="fas fa-check"></i>';
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+
+        // CHECK TRASH BUTTON
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+
+        todoList.appendChild(todoDiv);
+    });
+}
+
+function removeLocalTodos(todo) {
+    // provjeri da li ima nekaj u local storage-u
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    /// kaj zapravo dobivamo do sada sa removeLocalTodos
+    // 1.console.log(todo); // dobivamo klasu "todo fall"; todo je DIV
+    // 2. console.log(todo.children[0].innerText);
+    // [0] hvata <li>; // .inner.Text > hvata text u todo // ovo radimo da uhvatimo index od pobrisanog todo-a
+    // console.log(todos.indexOf("potato"));
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    // sada napravimo da izbriše iz arraya/storeage prema kliknutom todo
+    // ako sam kliknuo delete, izbriši ga i iz local storage-a
+
 }
